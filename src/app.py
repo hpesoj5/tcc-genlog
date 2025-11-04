2e49bb2ee2d82081b592dad042b470bf9a3f03a3
hpesoj5 <josephtante05@gmail.com>
2025-11-04 22:02:39 +0800

    completed autofillf function besides google sheet updating

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from generators import generators
 from sheet import authenticate
+import time
 
 def page_init():
     st.set_page_config(
@@ -11,15 +12,56 @@ def page_init():
             "Report a bug": "mailto:josephtante05@gmail.com",
         }
     )
+    if 'autofill' not in st.session_state:
+        st.session_state['autofill'] = False
     col1, col2 = st.columns([2, 3], gap='medium')
     return (col1, col2)
+        
+@st.dialog("Autofill in progress...", dismissible=False)
+def autofill(gens):
+    success = 0
+    error_container = st.container()
+    with st.empty():
+        for gen in gens:
+            try:
+                serial_no = generators[gen]
+                st.write(f"Updating sheet {serial_no}...")
+                    
+                # UPDATE GOOGLE SHEET   
+                    
+                print(f"{gen} log success!")
+                success += 1
+        
+            except Exception as e:
+                error_container.write(f"Error updating log for {gen}: {e}")
+                print(f"Error updating log for {gen}: {e}")
+        
+        st.write(f"Updated {success} sheets, could not update {len(gens) - success} sheets.")
+        
+    print(f"Updated {success} sheets, could not update {len(gens) - success} sheets.")
+    
+    st.session_state['autofill'] = False
+    time.sleep(1)
+    st.rerun()
+
+@st.dialog("Do you want to update the selected generators?")
+def confirm_autofill():
+    name = st.text_input(label="Name", label_visibility='hidden', width='stretch', placeholder='Enter name of authorising person')
+    no = st.button("No", width='stretch')
+    yes = st.button("Yes", width='stretch', type='primary')
+    if yes:
+        st.session_state['autofill'] = True
+        print("YES")
+        st.rerun()
+    elif no:
+        st.session_state['autofill'] = False
+        st.rerun()
 
 def main():
 
     # AUTHENTICATE GOOGLE SERVICE ACCOUNT TO UPDATE GOOGLE SHEET
     if 'sheet' not in st.session_state:
         st.session_state['sheet'] = authenticate()
-        print("Google service account authentication success!")
 
     # INIT PAGE
     col1, col2 = page_init()
@@ -49,26 +91,17 @@ def main():
     # LOGGING TOOLS
     with col2:
         if st.button("Autofill Logsheets", width='stretch'):
-            try:
-                for gen in options:
-                    serial_no = generators[gen]
-                    print(f"{gen} log success!")
-            
-            except Exception as e:
-                print(f"Error updating logs: {e}")
+            confirm_autofill()
         
+        if st.session_state['autofill']:
+            autofill(options)
+                        
         st.subheader("Add Manual Entry")
         
-        # entry_df = 
+        # entry_df = # TODO
         
         if st.button("Top up POL", width='stretch'):
-            try:
-                for gen in options:
-                    serial_no = generators[gen]
-                    print(f"{gen} log success!")
-            
-            except Exception as e:
-                print(f"Error updating logs: {e}")
+            pass # TODO
     
 if __name__ == '__main__':
     main()
--- src/sheet.py
+++ src/sheet.py
@@ -1,8 +1,23 @@
 import gspread
+from gspread.cell import Cell
 from google.oauth2.service_account import Credentials
 from pathlib import Path
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from generators import generators
+
+def isfloat(str: str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+def isdate(date: date):
+    try:
+        datetime.strptime(date, DATE_READ_FORMAT).date()
+        return True
+    except ValueError:
+        return False
 
 def authenticate():
     root_dir = Path(__file__).resolve().parent
@@ -14,14 +29,20 @@ def authenticate():
     
     client = gspread.authorize(creds)
     sheet = client.open('tcc-genlog')
+    print("Google service account authentication success!")
     
     return sheet
 
-DATE_WRITE_FORMAT = '%d/%m/%y'
+DATE_READ_FORMAT = '%d%m%y'
+DATE_WRITE_FORMAT = '%d%m%y'
 DATE_COL = 1
-EVENT_COL = 2
-REMARKS_COL = 3
-RUNTIME_COL = 4
+ADDRESS_COL = 2
+ENTRY_COL = 3
+RUNTIME_COL = 7
+READING_COL = 8
+FUEL_COL = 10
+NAME_COL = 13
+NUM_COLS = 13
 
 class Sheet:
     """
@@ -29,28 +50,125 @@ class Sheet:
     """
     def __init__(self, sheet: gspread.spreadsheet.Spreadsheet, gen: str):
         self.sheet = sheet.worksheet(gen)
+        self.data = {} # THIS IS TO STORE DATE AND READING VALUES, ENSURING THAT EACH COLUMN IS CALLED AT MOST ONCE EVERY AUTOFILL QUERY
 
     def get_latest_date(self):
-        date = self.sheet.col_values(DATE_COL)[-1]
-        return datetime.strptime(date, DATE_WRITE_FORMAT).date() if date != "Date" else None
+        """
+        Returns the latest entry date, together with the row number. Returns None if no date exists.
+        
+        API CALLS: 1
+        """
+        dates = self.sheet.col_values(DATE_COL)
+        self.data['dates'] = dates
+        rows = len(dates)
+        dates = [date for date in dates if isdate(date)]
+
+        return (datetime.strptime(dates[-1], DATE_READ_FORMAT).date(), rows) if len(dates) > 0 else None 
     
-    def get_latest_pol_date(self):
-        entries = zip(self.sheet.col_values(DATE_COL), self.sheet.col_values(EVENT_COL))
-        for entry_date, entry in reversed(entries):
-            if entry == "POL RECEIVED FROM TCC":
-                return datetime.strptime(entry_date, DATE_WRITE_FORMAT).date()
+    def get_latest_reading(self):
+        """
+        Returns the latest entry runtime reading. Returns None if no reading exists.
+        
+        API CALLS: 1
+        """
+        readings = self.sheet.col_values(READING_COL)
+        self.data['readings'] = readings
+        readings = [float(reading) for reading in readings if isfloat(reading)]
+
+        return readings[-1] if len(readings) > 0 else None
+        
+    def get_latest_pol_date_reading(self):
+        """
+        Returns the last POL top up date, as well as the reading after the top up. Returns None is no top up exists.
+        
+        API CALLS: 1-3
+        """
+        if "dates" not in self.data:
+            self.get_latest_date()
+        
+        if "readings" not in self.data:
+            self.get_latest_reading()
+            
+        entries = tuple(zip(self.data['dates'], self.sheet.col_values(ENTRY_COL), self.data['readings']))
+        for entry_date, entry, reading in reversed(entries):
+            if entry == "TOP UP POL":
+                return (datetime.strptime(entry_date, DATE_READ_FORMAT).date(), float(reading))
 
         return None
-
-    def get_latest_runtime(self):
-        res = self.sheet.col_values(RUNTIME_COL)[-1]
-        return res if res != "Stationary Runtime" else None
-    
-    # def autofill(self, end_date: date): # TO FINISH ONCE FORMAT OF LOGBOOK IS KNOWN
-    #     latest_date = self.get_latest_date()
-    #     latest_pol_date = self.get_latest_pol_date()
-    #     latest_runtime = self.get_latest_runtime()
+          
+    def autofill(self, gen: str, name: str="", end_date: date=date.today()):
+        """
+        Fills the spreadsheet with 0.5h runtime entries, automatically closes sheets every month and logs POL top ups
+        
+        API CALLS: 4
+        """
+        self.data.clear()
+        # GET DATA FROM SHEET
+        latest_date = self.get_latest_date() # 1 API CALL
+        latest_date, row = latest_date
+        latest_reading = self.get_latest_reading() # 1 API CALL
+        latest_pol_date_reading = self.get_latest_pol_date_reading() # 1 API CALL
+        
+        if latest_date == None or latest_reading == None:
+            print("Error: Need at least one runtime entry before autofill")
+            return False
+        
+        # SET PARAMETERS
+        increment = 0.5
+        
+        # SET RUNTIME SINCE LAST POL TOP UP AND LIMIT BETWEEN POL TOP UPS
+        runtime = 0.0 if latest_pol_date_reading == None else latest_reading - latest_pol_date_reading[1]
+        limit = 20.0
+        
+        # WRITE TO SPREADSHEET
+        cell_list = []
+        
+        while latest_date + timedelta(days=7) <= end_date:            
+            # CLOSE BOOK FOR PREVIOUS MONTH
+            if latest_date.month != (latest_date + timedelta(days=7)).month:
+                row += 1
+                cell_list.append(Cell(row=row, col=ADDRESS_COL, value=f"BOOK CLOSED FOR {latest_date.strftime('%b').upper()} {latest_date.strftime('%Y')}"))
+                
+            # INCREMENT EVERYTHING FOR EAS UPDATE
+            latest_date += timedelta(days=7)
+            latest_reading += increment
+            runtime += increment
+            row += 1
+            
+            cell_list.extend([
+                Cell(row=row, col=DATE_COL, value=date.strftime(latest_date, DATE_WRITE_FORMAT)),
+                Cell(row=row, col=ADDRESS_COL, value="JC1"),
+                Cell(row=row, col=ENTRY_COL, value="EAS"),
+                Cell(row=row, col=RUNTIME_COL, value=increment),
+                Cell(row=row, col=READING_COL, value=round(latest_reading, 2)),
+                Cell(row=row, col=NAME_COL, value=name)
+            ])
+            
+            # TOP UP POL
+            if runtime >= limit:
+                row += 1
+                cell_list.extend([
+                    Cell(row=row, col=DATE_COL, value=date.strftime(latest_date, DATE_WRITE_FORMAT)),
+                    Cell(row=row, col=ADDRESS_COL, value="POL RECEIVED FROM TCC"),
+                    Cell(row=row, col=ENTRY_COL, value="TOP UP POL"),
+                    Cell(row=row, col=READING_COL, value=round(latest_reading, 2)),
+                    Cell(row=row, col=FUEL_COL, value="60l"),
+                    Cell(row=row, col=NAME_COL, value=name)
+                ])
+                runtime = 0.0
+                
+        if len(cell_list) > 0:
+            self.sheet.update_cells(cell_list) # 1 API CALL
+            
+        print(f"Autofilled sheet {gen}!")
+        return True   
 
     # def write(self, event_date: date)                  TO WRITE MANUAL ENTRIES E.G. ROT 3
 
     # def top_up_pol(self, event_date: date):
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
